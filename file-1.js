bc6141caccab790c4b450bf4132b90ae7c6e99f0
Konstantin Mamonov <mamonov.kosttya@gmail.com>
2021-07-24 17:54:18 +0500

    add some changes again

--- file-1.js
+++ file-1.js
@@ -5,3 +5,5 @@ const fileChanged2 = 'new changes' // another file from user 2
 const newLine = 'NEW LINE' // from user 2
 
 const changes = 'some changes'
+
+const another_change = 'add it for this branch' // from user 1
